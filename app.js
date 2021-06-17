a7c3bd866e00e0188888bdd94eed46fef9028486
Danila <dendvo@ya.ru>
2021-06-17 11:25:30 -0700

    initial files

---
+++ app.js
@@ -0,0 +1,102 @@
+
+ const express = require("express");
+const parser = require("body-parser");
+ const app = express();
+ app.use(parser.urlencoded({extended: true}));
+ app.use(express.static(__dirname));
+
+   var deals = [];
+
+// app.set('views', __dirname);
+// app.engine('html', require('ejs').renderFile);
+
+ app.set('view engine', 'ejs')
+
+ app.get("/", function(req,res){
+  
+     var day = new Date();
+     const options = {
+         weekday: "long",
+         month: "long",
+         day: "numeric"
+     }
+     var today = day.toLocaleDateString("en-US", options);
+res.render("todolist", {theDay:today, newdeals:deals});
+
+
+ })
+
+ app.post("/", function (request, response){
+
+     var newDeal = request.body.newDeal
+     deals.push(newDeal);
+    response.redirect("/")
+ })
+
+
+
+
+ app.listen(3005, function(){
+     console.log("server is running on the port " + 3005);
+ })
+
+
+
+
+
+
+
+
+
+
+// if (today === 0) {
+//     kindOfDay = "Sunday"
+//     kindOfWeek = "Weekend!"
+// }
+// if (today === 1) {
+//     kindOfDay = "Monday =("
+//     kindOfWeek = "Weekday"
+// }
+// if (today === 2) {
+//     kindOfDay = "Tuesday"
+//     kindOfWeek = "Weekday"
+// }
+// if (today === 3) {
+//     kindOfDay = "Wednesday"
+//     kindOfWeek = "Weekday"
+// }
+// if (today === 4) {
+//     kindOfDay = "Thursday"
+//     kindOfWeek = "Weekday"
+// }
+// if (today === 5) {
+//     kindOfDay = "Friday"
+//     kindOfWeek = "Weekday"
+// }
+// if (today === 6) {
+//     kindOfDay = "Suturday"
+//     kindOfWeek = "Weekend!"
+// }
+// switch (today) {
+//     case 0:
+//         kindOfDay = "Sunday"    
+//         break;
+//         case 1:
+//             kindOfDay = "Monday"
+//             break;
+//             case 2:
+//                 kindOfDay = "Tuesday"
+//                 break;
+//                 case 3:
+//                     kindOfDay = "wednesday"
+//                     break;
+//                     case 4:
+//                         kindOfDay = "Thursday"
+//                         break; 
+//                         case 5:
+//                             kindOfDay = "Friday"
+//                             break;
+//                             case 6:
+//                                 kindOfDay = "Suturday"
+//                                         break;
+// }
